426277a5e8e1f329d3640a01544225f157caa0a1
Daniel <danbiwer@gmail.com>
2015-09-24 15:14:43 -0500

    php script for adding to db

---
+++ phpscripts/databaseUpdate.php
@@ -0,0 +1,23 @@
+<?php
+
+$key = $_REQUEST["key"];
+$tag = $_REQUEST["tag"];
+
+$dbhost = 'localhost';
+$dbuser = 'danbiwer_discogs';
+$dbpass = '63$kFOgUI~cU';
+$conn = mysql_connect($dbhost,$dbuser,$dbpass);
+if(! $conn){
+	die('Could not connect: ' . mysql_error());
+}
+
+$sql = "INSERT INTO `DiscogsLog` (`ID`, `keyword`, `tag`, `date`, `count`) VALUES (NULL, 'testing4', 'artist', NOW(), '3')";
+
+mysql_select_db('danbiwer_discogsSearchLog');
+$retval = mysql_query($sql, $conn);
+if(!retval){
+	die('Could not enter data: ' . mysql_error());
+}
+echo "Entered data successfully\n";
+mysql_close($conn);
+?>
